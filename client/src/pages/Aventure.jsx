5d9289820cc465f87da1183f8dc591761bc7abb2
Kevin-HA-KHA <nivekah27@gmail.com>
2024-03-06 15:46:58 +0100

    test intégration dragndrop

--- client/src/pages/Aventure.jsx
+++ client/src/pages/Aventure.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { useState } from 'react';
+import {DndContext} from '@dnd-kit/core';
+import {Draggable} from '../components/Draggable';
+import {Droppable} from '../components/Droppable';
 
 function Aventure() {
+  const [isDropped, setIsDropped] = useState(false);
+  const draggableMarkup = (
+    <Draggable>Drag me</Draggable>
+  );
+  
   return (
-    <div>
-        Aventure
-    </div>
-  )
+    <DndContext onDragEnd={handleDragEnd}>
+      {!isDropped ? draggableMarkup : null}
+      <Droppable>
+        {isDropped ? draggableMarkup : 'Drop here'}
+      </Droppable>
+    </DndContext>
+  );
+  
+  function handleDragEnd(event) {
+    if (event.over && event.over.id === 'droppable') {
+      setIsDropped(true);
+    }
+  }
 }
 
 
